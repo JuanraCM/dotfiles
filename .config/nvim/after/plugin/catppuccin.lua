75766a06f9192a43d7515cc48e78b80f4595849e
Juan Ramón Carretero Martín <juanra.carretero.m@astara.com>
2024-01-02 13:12:16 +0000

    feat(nvim): change catppuccin theme

--- .config/nvim/after/plugin/catppuccin.lua
+++ .config/nvim/after/plugin/catppuccin.lua
@@ -1,5 +1,5 @@
 require("catppuccin").setup({
-  flavour = "mocha",
+  flavour = "frappe",
 })
 
 vim.cmd.colorscheme "catppuccin"
--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -1,7 +1,7 @@
 require('lualine').setup {
   options = {
-    icons_enabled = false,
-    theme = 'horizon',
+    icons_enabled = true,
+    theme = 'catppuccin',
     component_separators = '|',
     section_separators = '',
   },
