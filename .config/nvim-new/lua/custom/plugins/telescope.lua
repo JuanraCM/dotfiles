a66648fef4900bb5159ea658e17c9670e04d6c05
Juan Ramón Carretero Martín <jrcarretero@tirant.com>
2024-11-27 13:11:36 +0000

    feat(nvim): basic sets and remaps

--- .config/nvim-new/lua/custom/plugins/telescope.lua
+++ .config/nvim-new/lua/custom/plugins/telescope.lua
@@ -7,7 +7,7 @@ return {
 
     vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = 'Telescope find files' })
     vim.keymap.set('n', '<leader>sa', builtin.live_grep, { desc = 'Telescope live grep' })
-    vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = 'Telescope buffers' })
+    vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = 'Telescope buffers' })
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = 'Telescope help tags' })
   end
 }
