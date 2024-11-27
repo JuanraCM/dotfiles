c127f490300f364592400e34a9e7b0b0f90af57f
Juan Ramón Carretero Martín <jrcarretero@tirant.com>
2024-11-27 13:11:36 +0000

    feat(nvim): basic sets and remaps

--- .config/nvim-new/lua/custom/remap.lua
+++ .config/nvim-new/lua/custom/remap.lua
@@ -3,3 +3,11 @@ vim.g.mapleader = ' '
 -- Leader inside a buffer
 vim.g.maplocalleader = ' '
 
+-- Remaps to center cursor when moving/searching in file
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+
+-- Paste without replacing current register
+vim.keymap.set('x', '<leader>p', [["_dP]], { desc = 'Paste without replacing current register' })
---
+++ .config/nvim-new/lua/custom/set.lua
@@ -0,0 +1,37 @@
+-- Set tab config to 2 spaces
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.bo.softtabstop = 2
+
+-- Built-in smartindent
+vim.opt.smartindent = true
+
+-- Make line numbers default
+vim.wo.number = true
+
+-- Enable mouse mode
+vim.o.mouse = 'a'
+
+-- Sync clipboard between OS and Neovim.
+vim.o.clipboard = 'unnamedplus'
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Case-insensitive searching UNLESS \C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.wo.signcolumn = 'yes'
+
+-- Decrease update time
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+
+-- NOTE: You should make sure your terminal supports this
+vim.o.termguicolors = true
+
+-- Leave lines of margin when scrolling
+vim.opt.scrolloff = 8
