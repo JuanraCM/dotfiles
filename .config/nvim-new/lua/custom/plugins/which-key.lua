7aca93e3beaa68d11533cd43016c86ccb42769f5
JuanraCM <juanracm.dev@gmail.com>
2024-12-01 14:11:35 +0000

    fix(nvim): text not highlighting in v-line mode and remove lazy weird stuff

--- .config/nvim-new/lua/custom/plugins/which-key.lua
+++ .config/nvim-new/lua/custom/plugins/which-key.lua
@@ -1,14 +1,9 @@
 return {
   "folke/which-key.nvim",
-  event = "VeryLazy",
   opts = {},
-  keys = {
-    {
-      "<leader>?",
-      function()
-        require("which-key").show({ global = false })
-      end,
-      desc = "Buffer Local Keymaps (which-key)",
-    }
-  }
+  config = function ()
+    vim.keymap.set('n', '<leader>?', function ()
+      require('which-key').show({ global = false })
+    end, { desc = "Buffer local keymaps" })
+  end
 }
