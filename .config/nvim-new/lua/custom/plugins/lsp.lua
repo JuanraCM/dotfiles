1a443b5248a4d97a43212ddeb7c3a367d4e647e6
JuanraCM <juanracm.dev@gmail.com>
2024-12-01 12:39:40 +0000

    feat(nvim): lualine LSP info & refactor mason config

--- .config/nvim-new/lua/custom/plugins/lsp.lua
+++ .config/nvim-new/lua/custom/plugins/lsp.lua
@@ -20,15 +20,29 @@ return {
     require('mason-lspconfig').setup()
     require('lazydev').setup()
 
+    local servers = {
+      lua_ls = {},
+      ruby_lsp = {},
+      rubocop = {}
+    }
+
     -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
-    require('lspconfig').lua_ls.setup({
-      capabilities = capabilities
-    })
-    require('lspconfig').rubocop.setup({
-      capabilities = capabilities
+    local lsp_config = require('mason-lspconfig')
+
+    lsp_config.setup({ ensure_installed = vim.tbl_keys(servers) })
+
+    lsp_config.setup_handlers({
+      function (server_name)
+        local server_setup = { capabilities = capabilities }
+        for key,value in pairs(servers[server_name]) do
+          server_setup[key] = value
+        end
+
+        require('lspconfig')[server_name].setup(server_setup)
+      end
     })
   end
 }
