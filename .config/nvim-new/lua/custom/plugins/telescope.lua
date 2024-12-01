d3cfeac58acdde1203c0ec3ddc0994309251cb21
JuanraCM <juanracm.dev@gmail.com>
2024-12-01 13:22:55 +0000

    feat(nvim): new telescope general keymaps & LSP

--- .config/nvim-new/lua/custom/plugins/telescope.lua
+++ .config/nvim-new/lua/custom/plugins/telescope.lua
@@ -29,8 +29,19 @@ return {
         builtin.find_files({ hidden = true })
       end, { desc = 'Telescope find files' })
       vim.keymap.set('n', '<leader>sa', builtin.live_grep, { desc = 'Telescope live grep' })
+      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = 'Telescope grep word under cursor' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = 'Telescope buffers' })
-      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = 'Telescope help tags' })
+      vim.keymap.set('n', '<leader>sg', builtin.git_files, { desc = 'Telescope find git files' })
+      vim.keymap.set('n', '<leader>sc', builtin.git_bcommits, { desc = "Telescope find buffer's git commits" })
+      vim.keymap.set('n', '<leader>sC', builtin.git_commits, { desc = "Telescope find git commits" })
+      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = 'Telescope find help tags' })
+      vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = 'Telescope find keymaps' })
+      vim.keymap.set('n', '<leader>/', function()
+        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+          winblend = 10,
+          previewer = false,
+        })
+      end, { desc = 'Fuzzily search in current buffer' })
     end
   },
   { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
--- .config/nvim-new/lua/custom/plugins/treesitter.lua
+++ .config/nvim-new/lua/custom/plugins/treesitter.lua
@@ -10,6 +10,7 @@ return {
       sync_install = false,
       auto_install = true,
       highlight = { enable = true },
+      indent = { enable = true },
       modules = {}
     })
   end
