0a7f7efbde8b1ace82f5c817a7245c46c5a57c83
JuanraCM <juanracm.dev@gmail.com>
2024-12-01 12:39:40 +0000

    feat(nvim): lualine LSP info & refactor mason config

--- .config/nvim-new/lua/custom/plugins/lualine.lua
+++ .config/nvim-new/lua/custom/plugins/lualine.lua
@@ -2,12 +2,30 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function ()
+    local function lsp_clients()
+      local icon = " "
+      local clients = vim.lsp.get_clients()
+      if next(clients) == nil then
+        return icon .. 'No LSP'
+      end
+
+      local client_names = {}
+      for i = 1, #clients, 1 do
+        table.insert(client_names, clients[i].name)
+      end
+
+      return icon .. table.concat(client_names, ', ')
+    end
+
     require('lualine').setup({
       options = {
         icons_enabled = true,
         theme = 'catppuccin',
         component_separators = '|',
         section_separators = ''
+      },
+      sections = {
+        lualine_y = { lsp_clients }
       }
     })
   end
