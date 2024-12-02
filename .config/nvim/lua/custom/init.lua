ed807c8817f184340805208b7fdc4e57824e9d5f
Juan Ramón Carretero Martín <jrcarretero@tirant.com>
2024-12-02 16:54:48 +0000

    feat(nvim): add custom tirant workspace config

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -1,3 +1,8 @@
 require("custom.set")
 require("custom.remap")
 require("custom.lazy")
+
+local cwd = vim.fn.getcwd()
+if string.find(cwd, "/Tirant/") then
+  require("custom.workspace.tirant")
+end
