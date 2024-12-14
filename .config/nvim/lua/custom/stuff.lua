ba8bfeee8191e9156eb26384291179685727dba8
JuanraCM <juanracm.dev@gmail.com>
2024-12-14 14:28:28 +0000

    feat(nvim): Exec lua code keymaps & improve Sandbox command

--- .config/nvim/lua/custom/stuff.lua
+++ .config/nvim/lua/custom/stuff.lua
@@ -3,15 +3,24 @@ vim.api.nvim_create_user_command("Q", function (evt)
   vim.cmd({ cmd = "bdelete", bang = evt.bang })
 end, { bang = true })
 
--- Creates a temporary sandbox buffer that executes Lua code
-vim.api.nvim_create_user_command("Sandbox", function (_)
+-- Remaps for executing code when editing Lua files
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("ExecLuaCodeKeymaps", { clear = true }),
+  pattern = "lua",
+  callback = function (evt)
+    vim.keymap.set("n", "<leader>x", ":so<CR>", { buffer = evt.buf, desc = "Run code" })
+    vim.keymap.set("v", "<leader>x", ":lua<CR>", { buffer = evt.buf, desc = "Run selected code" })
+  end
+})
+
+-- Creates a temporary sandbox buffer for a given language
+vim.api.nvim_create_user_command("Sandbox", function (evt)
+  local type = evt.fargs[1]
   local buf = vim.api.nvim_create_buf(true, false)
 
-  vim.api.nvim_buf_set_name(buf, "Sandbox")
-  vim.api.nvim_set_option_value("filetype", "lua", { buf = buf })
+  vim.api.nvim_buf_set_name(buf, "Sandbox" .. " (" .. type .. ")")
+  vim.api.nvim_set_option_value("filetype", type, { buf = buf })
   vim.api.nvim_set_option_value("buftype", "nofile", { buf = buf })
   vim.api.nvim_set_current_buf(buf)
+end, { nargs = 1 })
 
-  vim.keymap.set("n", "<leader>x", ":so<CR>", { buffer = buf, desc = "Sandbox: Run code" })
-  vim.keymap.set("v", "<leader>x", ":lua<CR>", { buffer = buf, desc = "Sandbox: Run selected code" })
-end, {})
