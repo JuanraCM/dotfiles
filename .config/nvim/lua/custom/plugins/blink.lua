07c97759e3e6e8beac7b016cb129dac84707c21d
JuanraCM <juanracm.dev@gmail.com>
2025-01-05 17:01:01 +0000

    feat(nvim): Fix codeium & lazydev for blink.cmp

--- .config/nvim/lua/custom/plugins/blink.lua
+++ .config/nvim/lua/custom/plugins/blink.lua
@@ -1,6 +1,14 @@
 return {
   'saghen/blink.cmp',
-  dependencies = 'rafamadriz/friendly-snippets',
+  dependencies = {
+    { 'saghen/blink.compat', opts = {} },
+    {
+      'Exafunction/codeium.nvim',
+      dependencies = 'nvim-lua/plenary.nvim',
+      opts = {}
+    },
+    'rafamadriz/friendly-snippets',
+  },
   version = 'v0.*',
   opts = {
     keymap = { preset = 'default' },
@@ -9,12 +17,22 @@ return {
       nerd_font_variant = 'mono'
     },
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer', 'lazydev' },
+      default = { 'lsp', 'path', 'snippets', 'buffer', 'lazydev', 'codeium' },
       providers = {
         snippets = {
           opts = {
             search_paths = { vim.fn.stdpath('config') .. '/lua/custom/snippets' }
           }
+        },
+        lazydev = {
+          name = 'LazyDev',
+          module = 'lazydev.integrations.blink',
+          score_offset = 100
+        },
+        codeium = {
+          name = 'codeium',
+          module = 'blink.compat.source',
+          score_offset = -3
         }
       }
     },
