61b809232aaf64588fbbed4c558d88f45d8c6fc3
Juan Ramón Carretero Martín <jrcarretero@tirant.com>
2025-01-08 12:54:49 +0000

    feat(nvim): Add custom telescope keymaps for oil.nvim

--- .config/nvim/lua/custom/plugins/oil.lua
+++ .config/nvim/lua/custom/plugins/oil.lua
@@ -1,17 +1,32 @@
 return {
-  'stevearc/oil.nvim',
+  "stevearc/oil.nvim",
   ---@module 'oil'
   ---@type oil.SetupOpts
   opts = {},
   -- Optional dependencies
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  config = function ()
-    require('oil').setup({
+  config = function()
+    require("oil").setup({
       view_options = {
-        show_hidden = true
-      }
+        show_hidden = true,
+      },
     })
 
-    vim.keymap.set('n', '-', '<CMD>Oil --float<CR>', { desc = "Open parent directory" })
-  end
+    vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      group = vim.api.nvim_create_augroup("OilTelescope", { clear = true }),
+      pattern = "oil",
+      callback = function(evt)
+        local cwd = vim.api.nvim_buf_get_name(0):gsub("^oil://", "")
+
+        vim.keymap.set("n", "<leader>sf", function()
+          require("telescope.builtin").find_files({ cwd = cwd, hidden = true })
+        end, { buffer = evt.buf, desc = "Telescope find files" })
+        vim.keymap.set("n", "<leader>sa", function()
+          require("custom.telescope").live_multigrep({ cwd = cwd })
+        end, { buffer = evt.buf, desc = "Telescope live multigrep" })
+      end,
+    })
+  end,
 }
