d946542ca5b10ccfb52c14b93c779952d2ea4555
Juan Ramón Carretero Martín <jrcarretero@tirant.com>
2025-01-08 12:54:25 +0000

    feat(nvim): Remove codeium

--- .config/nvim/lua/custom/plugins/blink.lua
+++ .config/nvim/lua/custom/plugins/blink.lua
@@ -1,40 +1,29 @@
 return {
-  'saghen/blink.cmp',
+  "saghen/blink.cmp",
   dependencies = {
-    { 'saghen/blink.compat', opts = {} },
-    {
-      'Exafunction/codeium.nvim',
-      dependencies = 'nvim-lua/plenary.nvim',
-      opts = {}
-    },
-    'rafamadriz/friendly-snippets',
+    "rafamadriz/friendly-snippets",
   },
-  version = 'v0.*',
+  version = "v0.*",
   opts = {
-    keymap = { preset = 'default' },
+    keymap = { preset = "default" },
     appearance = {
       use_nvim_cmp_as_default = true,
-      nerd_font_variant = 'mono'
+      nerd_font_variant = "mono",
     },
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer', 'lazydev', 'codeium' },
+      default = { "lsp", "path", "snippets", "buffer", "lazydev" },
       providers = {
         snippets = {
           opts = {
-            search_paths = { vim.fn.stdpath('config') .. '/lua/custom/snippets' }
-          }
+            search_paths = { vim.fn.stdpath("config") .. "/lua/custom/snippets" },
+          },
         },
         lazydev = {
-          name = 'LazyDev',
-          module = 'lazydev.integrations.blink',
-          score_offset = 100
+          name = "LazyDev",
+          module = "lazydev.integrations.blink",
+          score_offset = 100,
         },
-        codeium = {
-          name = 'codeium',
-          module = 'blink.compat.source',
-          score_offset = -3
-        }
-      }
+      },
     },
     signature = { enabled = true },
     completion = {
@@ -43,11 +32,11 @@ return {
           columns = {
             { "label", "label_description", gap = 1 },
             { "kind_icon", "kind", "source_name", gap = 1 },
-          }
-        }
+          },
+        },
       },
-      documentation = { auto_show = true }
+      documentation = { auto_show = true },
     },
   },
-  opts_extend = { "sources.default" }
+  opts_extend = { "sources.default" },
 }
