ac3401480c62f6757cd07920fc53609e36cf1456
Juan Ramón Carretero Martín <jrcarretero@tirant.com>
2025-01-09 13:30:19 +0000

    feat(nvim): keymap to yank file path to clipboard

--- .config/nvim/lua/custom/remap.lua
+++ .config/nvim/lua/custom/remap.lua
@@ -1,25 +1,27 @@
-
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 -- Leader inside a buffer
-vim.g.maplocalleader = ' '
+vim.g.maplocalleader = " "
 
 -- Disables spacebar in normal and visual modes
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
 -- Remaps to center cursor when moving/searching in file
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
-vim.keymap.set('n', 'n', 'nzzzv')
-vim.keymap.set('n', 'N', 'Nzzzv')
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
 
 -- Paste without replacing current register
-vim.keymap.set('x', '<leader>p', [["_dP]], { desc = 'Paste without replacing current register' })
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Paste without replacing current register" })
 
 -- Buffer navigation
-vim.keymap.set('n', 'gb', ':bnext<cr>', { desc = 'Go to next buffer' })
-vim.keymap.set('n', 'gp', ':bprevious<cr>', { desc = 'Go to previous buffer' })
+vim.keymap.set("n", "gb", ":bnext<cr>", { desc = "Go to next buffer" })
+vim.keymap.set("n", "gp", ":bprevious<cr>", { desc = "Go to previous buffer" })
 
 -- Toggle relative numbers
-vim.keymap.set('n', '<leader>tr', function ()
+vim.keymap.set("n", "<leader>tr", function()
   vim.wo.relativenumber = not vim.wo.relativenumber
-end, { desc = 'Toggle relative numbers' })
+end, { desc = "Toggle relative numbers" })
+
+-- Yank relative file path to system clipboard
+vim.keymap.set("n", "<leader>yf", ':let @+ = expand("%")<cr>', { desc = "Yank file path to clipboard" })
