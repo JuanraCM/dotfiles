b5da20401ca87b274c852286048c5a18392fe3e3
Juan Ramón Carretero Martín <jrcarretero@tirant.com>
2025-01-17 13:58:12 +0000

    feat(nvim): add gp.nvim plugin

---
+++ .config/nvim/lua/custom/plugins/gpt.lua
@@ -0,0 +1,26 @@
+return {
+  "robitx/gp.nvim",
+  config = function()
+    require("gp").setup({
+      openai_api_key = "",
+      providers = {
+        openai = {},
+        googleai = {
+          endpoint = "https://generativelanguage.googleapis.com/v1beta/models/{{model}}:generateContent?key={{secret}}",
+          secret = os.getenv("GOOGLEAI_API_KEY"),
+        },
+      },
+      agents = {
+        { name = "ChatGemini", disable = true },
+        {
+          provider = "googleai",
+          name = "Gemini",
+          chat = true,
+          command = false,
+          model = { model = "gemini-1.5-flash" },
+          system_prompt = require("gp.defaults").chat_system_prompt,
+        },
+      },
+    })
+  end,
+}
