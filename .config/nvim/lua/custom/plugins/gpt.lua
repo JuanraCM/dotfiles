7f3ee5f07b7858afa8aa8beacac9c88d4affb7e5
JuanraCM <juanracm.dev@gmail.com>
2025-01-26 17:45:54 +0000

    feat(nvim): add deepseek agents to gp config

--- .config/nvim/lua/custom/plugins/gpt.lua
+++ .config/nvim/lua/custom/plugins/gpt.lua
@@ -4,20 +4,42 @@ return {
     require("gp").setup({
       openai_api_key = "",
       providers = {
-        openai = {},
+        openai = {
+          endpoint = "https://api.deepseek.com/chat/completions",
+          secret = os.getenv("DEEPSEEK_API_KEY"),
+        },
         googleai = {
           endpoint = "https://generativelanguage.googleapis.com/v1beta/models/{{model}}:streamGenerateContent?key={{secret}}",
           secret = os.getenv("GOOGLEAI_API_KEY"),
         },
       },
       agents = {
-        { name = "ChatGemini", disable = true },
+        { name = "ChatGPT4o", disable = true },
+        { name = "ChatGPT4o-mini", disable = true },
+        { name = "CodeGPT4o", disable = true },
+        { name = "CodeGPT4o-mini", disable = true },
         {
           provider = "googleai",
-          name = "Gemini",
+          name = "ChatGemini",
           chat = true,
           command = false,
           model = { model = "gemini-1.5-flash" },
+          system_prompt = "You are a general AI assistant.",
+        },
+        {
+          provider = "openai",
+          name = "DeepSeekChat",
+          chat = true,
+          command = false,
+          model = { model = "deepseek-chat", temperature = 0.0 },
+          system_prompt = "You are a general AI assistant.",
+        },
+        {
+          provider = "openai",
+          name = "DeepSeekReasoner",
+          chat = true,
+          command = false,
+          model = { model = "deepseek-reasoner" },
           system_prompt = "You are a general AI assistant.",
         },
       },
