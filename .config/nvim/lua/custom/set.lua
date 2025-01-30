f617b42d895718a43a4ad731e717c0d058bc0f52
Juan Ramón Carretero Martín <jrcarretero@tirant.com>
2025-01-30 11:38:47 +0000

    feat(nvim): enable relativenumbers by default

--- .config/nvim/lua/custom/set.lua
+++ .config/nvim/lua/custom/set.lua
@@ -17,6 +17,9 @@ vim.o.hlsearch = false
 -- Make line numbers default
 vim.wo.number = true
 
+-- Use relative numbers by default
+vim.wo.relativenumber = true
+
 -- Enable mouse mode
 vim.o.mouse = "a"
 
