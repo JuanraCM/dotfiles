19d627638a81a59006578b866b8a848050a1ec0c
Juan Ramón Carretero Martín <jrcarretero@tirant.com>
2025-01-30 11:13:49 +0000

    feat(nvim): remap to open fugitive in vertical mode

--- .config/nvim/lua/custom/plugins/git.lua
+++ .config/nvim/lua/custom/plugins/git.lua
@@ -1,13 +1,14 @@
 return {
-  'tpope/vim-fugitive',
+  "tpope/vim-fugitive",
   {
-    'lewis6991/gitsigns.nvim',
-    config = function ()
-      local gitsigns = require('gitsigns')
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      local gitsigns = require("gitsigns")
 
       gitsigns.setup()
 
-      vim.keymap.set('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = 'Toggle current line blame' })
-    end
-  }
+      vim.keymap.set("n", "<leader>tb", gitsigns.toggle_current_line_blame, { desc = "Toggle current line blame" })
+      vim.keymap.set("n", "<c-g>", ":vertical G<cr>", { desc = "Open git status in a vertical buffer" })
+    end,
+  },
 }
