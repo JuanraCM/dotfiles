2aba5a6f5c5ae0a7dc393a7562e03b33549c54bc
Juan Ramón Carretero Martín <jrcarretero@tirant.com>
2025-02-11 13:06:54 +0000

    feat(nvim): toggle linters keymap

--- .config/nvim/lua/custom/plugins/lint.lua
+++ .config/nvim/lua/custom/plugins/lint.lua
@@ -1,27 +1,41 @@
 return {
-  'mfussenegger/nvim-lint',
-  config = function ()
-    local lint = require('lint')
-    local cutils = require('custom.utils')
+  "mfussenegger/nvim-lint",
+  config = function()
+    local lint = require("lint")
+    local cutils = require("custom.utils")
 
     lint.linters_by_ft = {
-      ruby = { 'rubocop' }
+      ruby = { "rubocop" },
     }
 
-    lint.linters.rubocop.cmd = cutils.script_path('docker-rubocop')
+    lint.linters.rubocop.cmd = cutils.script_path("docker-rubocop")
     lint.linters.rubocop.args = {
-      '--format',
-      'json',
-      '--force-exclusion',
-      '--stdin',
-      function() return vim.fn.expand('%') end,
+      "--format",
+      "json",
+      "--force-exclusion",
+      "--stdin",
+      function()
+        return vim.fn.expand("%")
+      end,
     }
 
-    vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost' }, {
-      group = vim.api.nvim_create_augroup('NvimLint', { clear = true }),
-      callback = function ()
-        lint.try_lint()
+    local lint_enabled = true
+
+    vim.keymap.set("n", "<leader>tl", function()
+      lint_enabled = not lint_enabled
+
+      if not lint_enabled then
+        vim.diagnostic.reset(nil, 0)
       end
+    end, { desc = "Toggle linters" })
+
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
+      group = vim.api.nvim_create_augroup("NvimLint", { clear = true }),
+      callback = function()
+        if lint_enabled then
+          lint.try_lint()
+        end
+      end,
     })
-  end
+  end,
 }
