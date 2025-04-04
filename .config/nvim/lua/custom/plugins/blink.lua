ec384d29a84ed9bc914282593427d9f01018dd62
Juan Ramón Carretero Martín <jrcarretero@tirant.com>
2025-04-04 12:02:08 +0100

    feat(nvim): remove luasnip & use blink + native snippets expand

--- .config/nvim/lua/custom/plugins/blink.lua
+++ .config/nvim/lua/custom/plugins/blink.lua
@@ -1,49 +1,69 @@
 return {
   "saghen/blink.cmp",
   version = "1.*",
-  opts = {
-    fuzzy = { implementation = "rust" },
-    keymap = { preset = "default" },
-    appearance = {
-      use_nvim_cmp_as_default = true,
-      nerd_font_variant = "mono",
-    },
-    snippets = { preset = "luasnip" },
-    sources = {
-      default = { "lsp", "path", "snippets", "buffer", "lazydev" },
-      per_filetype = {
-        sql = { "snippets", "dadbod", "buffer" },
-        mysql = { "snippets", "dadbod", "buffer" },
-        codecompanion = { "codecompanion" },
+  dependencies = {
+    "rafamadriz/friendly-snippets",
+  },
+  config = function()
+    local custom_vars = {
+      lazy = {
+        BDD_TASK_ID = function()
+          local branch_name = vim.system({ "git", "branch", "--show-current" }, { text = true }):wait().stdout
+          return branch_name and vim.split(branch_name, "-")[2] or "UNKNOWN"
+        end,
       },
-      providers = {
-        lsp = { async = true },
-        lazydev = {
-          name = "LazyDev",
-          module = "lazydev.integrations.blink",
-          score_offset = 100,
-        },
-        snippets = {
-          score_offset = -2,
-        },
-        dadbod = {
-          name = "Dadbod",
-          module = "vim_dadbod_completion.blink",
-        },
+      eager = {},
+    }
+
+    local registry = require("blink.cmp.sources.snippets.default.registry")
+    local aggregated_vars = vim.tbl_deep_extend("force", registry.builtin_vars, custom_vars)
+    registry.builtin_vars = aggregated_vars
+
+    require("blink.cmp").setup({
+      fuzzy = { implementation = "rust" },
+      keymap = { preset = "default" },
+      appearance = {
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = "mono",
       },
-    },
-    signature = { enabled = true },
-    completion = {
-      menu = {
-        draw = {
-          columns = {
-            { "label", "label_description", gap = 1 },
-            { "kind_icon", "kind", "source_name", gap = 1 },
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer", "lazydev" },
+        per_filetype = {
+          sql = { "snippets", "dadbod", "buffer" },
+          mysql = { "snippets", "dadbod", "buffer" },
+          codecompanion = { "codecompanion" },
+        },
+        providers = {
+          lsp = { async = true },
+          lazydev = {
+            name = "LazyDev",
+            module = "lazydev.integrations.blink",
+            score_offset = 100,
+          },
+          snippets = {
+            score_offset = -2,
+            opts = {
+              search_paths = { vim.fn.stdpath("config") .. "/lua/custom/snippets" },
+            },
+          },
+          dadbod = {
+            name = "Dadbod",
+            module = "vim_dadbod_completion.blink",
           },
         },
       },
-      documentation = { auto_show = true },
-    },
-  },
-  opts_extend = { "sources.default" },
+      signature = { enabled = true },
+      completion = {
+        menu = {
+          draw = {
+            columns = {
+              { "label", "label_description", gap = 1 },
+              { "kind_icon", "kind", "source_name", gap = 1 },
+            },
+          },
+        },
+        documentation = { auto_show = true },
+      },
+    })
+  end,
 }
--- .config/nvim/queries/lua/injections.scm
+++
@@ -1,8 +0,0 @@
-; Highlight LuaSnip fmt code template
-((function_call
-  name: _ @fmt_call
-  arguments: (arguments
-    (string
-      content: _ @injection.content)))
-  (#eq? @fmt_call "fmt")
-  (#set-lang-from-filename!))
