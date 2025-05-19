6f791edd5124b0971e8bf3229a332f4162aa2ed4
Juan Ramón Carretero Martín <jrcarretero@tirant.com>
2025-05-19 08:36:57 +0200

    feat(nvim): add copilot & mcphub to codecompanion

--- .config/nvim/lua/custom/plugins/ai.lua
+++ .config/nvim/lua/custom/plugins/ai.lua
@@ -1,6 +1,11 @@
 return {
   "olimorris/codecompanion.nvim",
   dependencies = {
+    "github/copilot.vim",
+    {
+      "ravitemer/mcphub.nvim",
+      build = "npm install -g mcp-hub@latest",
+    },
     {
       "MeanderingProgrammer/render-markdown.nvim",
       ft = { "markdown", "codecompanion" },
@@ -9,25 +14,31 @@ return {
     "nvim-treesitter/nvim-treesitter",
   },
   config = function()
+    require("mcphub").setup()
     require("codecompanion").setup({
+      extensions = {
+        mcphub = {
+          callback = "mcphub.extensions.codecompanion",
+          opts = {
+            show_result_in_chat = true,
+            make_vars = true,
+            make_slash_commands = true,
+          },
+        },
+      },
       adapters = {
         gemini = function()
           return require("codecompanion.adapters").extend("gemini", {
-            env = {
-              model = "gemini-2.5-pro-exp-03-25",
+            schema = {
+              model = {
+                default = "gemini-2.5-pro-preview-05-06",
+              },
             },
           })
         end,
       },
-      strategies = {
-        chat = {
-          adapter = "gemini",
-        },
-        inline = {
-          adapter = "gemini",
-        },
-      },
     })
+    require("custom.codecompanion-progress"):init()
 
     vim.keymap.set("n", "<c-c>", ":CodeCompanionChat Toggle<cr>", { desc = "Toggle CodeCompanionChat" })
     vim.keymap.set("v", "<c-c>", ":CodeCompanionChat Add<cr>", { desc = "Open CodeCompanionChat with selected code" })
