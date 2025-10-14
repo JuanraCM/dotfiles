bdd70a6a39a1d6b851fbec939b4d1ef877d89632
Juan Ramón Carretero Martín <jrcarretero@tirant.com>
2025-10-14 19:51:41 +0100

    feat(nvim): tasks plugin

---
+++ .config/nvim/plugin/tasks.lua
@@ -0,0 +1,67 @@
+---@module "snacks"
+
+local TASKS_DIR = vim.fs.joinpath(vim.fn.stdpath("data"), "tasks")
+local TASK_TEMPLATE = { "", "## Context", "", "- TODO", "", "## Task", "", "- TODO", "" }
+
+local function ensure_tasks_dir()
+  if vim.fn.isdirectory(TASKS_DIR) == 0 then
+    vim.fn.mkdir(TASKS_DIR)
+  end
+end
+
+local function list_tasks()
+  local tasks = vim.fn.readdir(TASKS_DIR)
+  if vim.tbl_isempty(tasks) then
+    print("No tasks found.")
+    return
+  end
+
+  Snacks.picker.files({
+    title = "Tasks",
+    cwd = TASKS_DIR,
+    win = {
+      input = {
+        keys = {
+          ["<c-x>"] = { "delete_files", mode = { "n", "i" } },
+        },
+      },
+    },
+  })
+end
+
+local function new_task()
+  Snacks.input({ prompt = "Enter task name" }, function(task_name)
+    if not task_name or task_name == "" then
+      print("Task name cannot be empty.")
+      return
+    end
+
+    local current_date = os.date("%Y-%m-%d")
+    local task_file = TASKS_DIR .. "/" .. current_date .. "-" .. task_name .. ".md"
+    if vim.fn.filereadable(task_file) == 1 then
+      print("Task already exists.")
+      return
+    end
+
+    vim.fn.writefile(TASK_TEMPLATE, task_file)
+    vim.cmd("edit " .. task_file)
+  end)
+end
+
+vim.api.nvim_create_user_command("Tasks", function(cmd)
+  ensure_tasks_dir()
+
+  if cmd.args == "" or cmd.args == "list" then
+    list_tasks()
+  elseif cmd.args == "new" then
+    new_task()
+  else
+    print("Usage: :Tasks [list|new]")
+  end
+end, {
+  desc = "AI Task manager",
+  nargs = "?",
+  complete = function()
+    return { "list", "new" }
+  end,
+})
