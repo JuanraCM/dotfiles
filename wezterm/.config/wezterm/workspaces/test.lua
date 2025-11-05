5fe31f7fe92bb65d47f655c9126e9e3f01f4b848
Juan Ramón Carretero Martín <jrcarretero@tirant.com>
2025-11-05 19:41:48 +0000

    chore: update gitignore

--- wezterm/.config/wezterm/workspaces/test.lua
+++
@@ -1,9 +0,0 @@
-return {
-  label = "Test",
-  setup = function(mux_window, home_dir)
-    mux_window:spawn_tab({ cwd = home_dir })
-
-    mux_window:active_tab():set_title("Test")
-    mux_window:active_pane():send_text("echo 'This is a test workspace'\n")
-  end,
-}
