5a3969cd44b8e368124293317b3bc7d13d49a2e0
JuanraCM <juanracm.dev@gmail.com>
2025-12-25 15:24:01 +0000

    fix(nvim): set treesitter branch to master

--- nvim/.config/nvim/lua/custom/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/custom/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   dependencies = "nvim-treesitter/nvim-treesitter-textobjects",
+  branch = "master",
   build = ":TSUpdate",
   config = function()
     local configs = require("nvim-treesitter.configs")
