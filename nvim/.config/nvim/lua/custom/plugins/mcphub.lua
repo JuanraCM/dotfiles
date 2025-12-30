4cfce0545457eebb24bb97eccbc2fb3432cb27ac
Juan Ramón Carretero Martín <jrcarretero@tirant.com>
2025-12-30 12:18:55 +0000

    fix(nvim): mcphub bundled binary not detected

--- nvim/.config/nvim/lua/custom/plugins/mcphub.lua
+++ nvim/.config/nvim/lua/custom/plugins/mcphub.lua
@@ -3,6 +3,8 @@ return {
   dependencies = { "nvim-lua/plenary.nvim" },
   build = "bundled_build.lua",
   config = function()
-    require("mcphub").setup({})
+    require("mcphub").setup({
+      use_bundled_binary = true,
+    })
   end,
 }
