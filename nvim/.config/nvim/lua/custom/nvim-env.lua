26831e4c0ef43a4fbe7734d10990e0e79f530f23
JuanraCM <juanracm.dev@gmail.com>
2026-01-05 17:28:34 +0000

    feat(nvim): make nvim-env plugin not load default values

--- nvim/.config/nvim/lua/custom/nvim-env.lua
+++ nvim/.config/nvim/lua/custom/nvim-env.lua
@@ -1,21 +1,17 @@
 local ENV_FILE = ".nvim-env.json"
-local ENV_VARS = {
-  DOCKER_CONTAINER = "app",
-  DISABLE_LSP_SERVERS = "",
-}
+local ENV_VARS = { "DOCKER_CONTAINER", "DISABLE_LSP_SERVERS" }
 
 local load_env = function()
-  local env_config = {}
-
-  if vim.fn.filereadable(ENV_FILE) == 1 then
-    local lines = vim.fn.readfile(ENV_FILE)
-    local content = table.concat(lines, "\n")
-
-    env_config = vim.json.decode(content)
+  if vim.fn.filereadable(ENV_FILE) == 0 then
+    return
   end
 
-  for key, default_value in pairs(ENV_VARS) do
-    vim.env[key] = env_config[key] or default_value
+  local lines = vim.fn.readfile(ENV_FILE)
+  local content = table.concat(lines, "\n")
+  local env_config = vim.json.decode(content)
+
+  for _, key in ipairs(ENV_VARS) do
+    vim.env[key] = env_config[key]
   end
 end
 
