40db9e87b930b76f4c8eb0216befaea76c52a5e2
Juan Ramón Carretero Martín <jrcarretero@tirant.com>
2026-01-16 11:12:40 +0000

    feat(nvim): swap sidekick.nvim with opencode.nvim

---
+++ nvim/.config/nvim/lua/custom/plugins/opencode.lua
@@ -0,0 +1,35 @@
+return {
+  "NickvanDyke/opencode.nvim",
+  config = function()
+    ---@type opencode.Opts
+    vim.g.opencode_opts = {
+      provider = {
+        enabled = "wezterm",
+      },
+      wezterm = {
+        direction = "bottom",
+        top_level = false,
+        percent = 50,
+      },
+    }
+
+    vim.o.autoread = true
+
+    vim.keymap.set({ "n" }, "<c-o>", function()
+      require("opencode").ask("@this: ", { submit = true })
+    end, { desc = "Ask opencode…" })
+    vim.keymap.set({ "n", "x" }, "<C-p>", function()
+      require("opencode").select()
+    end, { desc = "Execute opencode action…" })
+    vim.keymap.set({ "n", "t" }, "<C-.>", function()
+      require("opencode").toggle()
+    end, { desc = "Toggle opencode" })
+
+    vim.keymap.set({ "n", "x" }, "go", function()
+      return require("opencode").operator("@this ")
+    end, { desc = "Add range to opencode", expr = true })
+    vim.keymap.set("n", "goo", function()
+      return require("opencode").operator("@this ") .. "_"
+    end, { desc = "Add line to opencode", expr = true })
+  end,
+}
