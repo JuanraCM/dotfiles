c5bb6b89b1693ffb5da702993ffb954e2a1d81f3
JuanraCM <juanracm.dev@gmail.com>
2026-08-23 13:34:00 +0100

    chore(nvim): remove custom dockerized rubocop formatting

--- nvim/.config/nvim/after/ftplugin/ruby.lua
+++ nvim/.config/nvim/after/ftplugin/ruby.lua
@@ -1,12 +1,3 @@
-vim.api.nvim_buf_create_user_command(0, "RubocopFixAll", function(evt)
-  local formatter = evt.bang and "rubocop_unsafe" or "rubocop"
-
-  require("conform").format({
-    formatters = { formatter },
-    timeout_ms = 10000,
-  })
-end, { bang = true, desc = "Fix all rubocop offenses" })
-
 vim.keymap.set("v", "<leader>m", function()
   require("custom.ruby-mp").monkeypatch()
 end, { buffer = true, desc = "Monkeypatch ruby class" })
--- nvim/.config/nvim/lua/custom/plugins/format.lua
+++ nvim/.config/nvim/lua/custom/plugins/format.lua
@@ -4,23 +4,6 @@ return {
   config = function()
     local conform = require("conform")
 
-    local rubocop_args = function(safe_mode)
-      return function()
-        return {
-          vim.env.DOCKER_CONTAINER,
-          "bundle",
-          "exec",
-          "rubocop",
-          safe_mode and "-a" or "-A",
-          "-f",
-          "quiet",
-          "--stderr",
-          "--stdin",
-          vim.fn.expand("%:."),
-        }
-      end
-    end
-
     conform.setup({
       format_on_save = {
         lsp_format = "fallback",
@@ -28,17 +11,6 @@ return {
       },
       formatters_by_ft = {
         lua = { "stylua" },
-      },
-      formatters = {
-        rubocop = {
-          command = require("custom.utils").script_path("docker-cmd"),
-          args = rubocop_args(true),
-        },
-        rubocop_unsafe = {
-          command = require("custom.utils").script_path("docker-cmd"),
-          args = rubocop_args(false),
-          exit_codes = { 0, 1 },
-        },
       },
     })
   end,
