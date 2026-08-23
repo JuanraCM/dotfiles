08c9c63a0b000d25202ecbb3cf6bc243df8f57c0
JuanraCM <juanracm.dev@gmail.com>
2026-08-23 13:31:52 +0100

    feat(nvim): vim-test non-dockerized projects support

--- nvim/.config/nvim/lua/custom/plugins/test.lua
+++ nvim/.config/nvim/lua/custom/plugins/test.lua
@@ -1,22 +1,21 @@
 return {
   "vim-test/vim-test",
   config = function()
-    local function docker_transform(cmd)
-      return table.concat({
-        require("custom.utils").script_path("docker-cmd"),
-        vim.env.DOCKER_CONTAINER,
-        cmd,
-      }, " ")
-    end
-
-    vim.g["test#custom_transformations"] = { docker = docker_transform }
-    vim.g["test#transformation"] = "docker"
     vim.g["test#strategy"] = "neovim_sticky"
-
     vim.g["test#neovim#term_position"] = "vert"
 
-    -- Jest config
-    vim.g["test#javascript#jest#executable"] = "yarn test"
+    if vim.env.DOCKER_CONTAINER then
+      vim.g["test#custom_transformations"] = {
+        docker = function(cmd)
+          return table.concat({
+            require("custom.utils").script_path("docker-cmd"),
+            vim.env.DOCKER_CONTAINER,
+            cmd,
+          }, " ")
+        end
+      }
+      vim.g["test#transformation"] = "docker"
+    end
 
     vim.keymap.set("n", "<leader>rf", ":TestFile<cr>", { desc = "Run test file" })
     vim.keymap.set("n", "<leader>rt", ":TestNearest<cr>", { desc = "Run nearest test" })
